fae716241eaeeff55e318cef3a9092f6149a9880
SUNNY BHAVEEN CHANDRA <34603575+c17hawke@users.noreply.github.com>
2023-04-29 16:20:15 +0530

    updated to py3.9

--- init_setup.sh
+++ init_setup.sh
@@ -1,6 +1,6 @@
 echo [$(date)]: "START"
-echo [$(date)]: "Creating conda env with python 3.8" # change py version as per your need
-conda create --prefix ./env python=3.8 -y
+echo [$(date)]: "Creating conda env with python 3.9" # change py version as per your need
+conda create --prefix ./env python=3.9 -y
 echo [$(date)]: "activate env"
 source activate ./env
 echo [$(date)]: "installing the requirements" 
